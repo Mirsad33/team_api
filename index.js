1447d23d41598b4723223bceea6ab2ce1d831128
Mirsad33 <mabedinoski3387@gmail.com>
2024-04-05 13:58:46 -0400

    finished our es6 and our es7 overview

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
 
 
-function printSomething(str) {
-    console.log(str)
+// function printSomething(str) {
+//     console.log(str)
+// }
+
+// printSomething('some string')
+
+// const add = (num1, num2) => num1 + num2
+
+
+// const sum = add(5, 20)
+
+// console.log(sum)
+
+// function printStudents(...students) {
+//     console.log(students)
+// }
+
+// printStudents('Will', 'Mirsad', 'Muhsin', 'Trevor', 'Juan', 'Alice')
+
+// const fruits = ['orange', 'apple']
+
+// const copy = ['kiwi', ...fruits, 'banana']
+
+// const clone = {...data}
+// console.log(clone)
+
+// const data = {
+//     name: 'JD',
+//     age: 44
+// }
+
+// const { name, age, blah } = data
+
+// console.log(data)
+
+function someFunc({ age }, str, num) {
+    console.log(num)
 }
 
-printSomething('some string')
+someFunc({ name: 'Bob', age: 99 }, 'another', 15)
+
+
+
+
+
+
+
+
