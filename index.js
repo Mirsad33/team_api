349c496f9e811bed1150d1feb6a6d5109a79b945
Mirsad33 <mabedinoski3387@gmail.com>
2024-04-05 13:11:10 -0400

    base

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-const args = process.argv
 
-// console.log('browser stuff' .document, window)
-const num1 =args[2]
-const num2 = args[3]
 
-console.log('The sum is;', +num1 + +num2)
+function printSomething(str) {
+    console.log(str)
+}
 
-// Get two number arguments from the user's command list (process.argv)
-
-// Add 
+printSomething('some string')
