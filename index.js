6519778f3ae7411e96869729b799cd0cbb011d23
Mirsad33 <mabedinoski3387@gmail.com>
2024-04-07 22:56:27 -0400

    read-write

--- index.js
+++ index.js
@@ -1,45 +1,19 @@
+const fs = require('fs')
+
+fs.readFile('./students.txt', 'utf8', (err, data) => {
+  if (err) {
+    return console.log(err)
+  }
+
+  const names = data.split()
+
+  for (let name of data) {
+    console.log(name)
+  }
+})
 
 
-// function printSomething(str) {
-//     console.log(str)
-// }
-
-// printSomething('some string')
-
-// const add = (num1, num2) => num1 + num2
-
-
-// const sum = add(5, 20)
-
-// console.log(sum)
-
-// function printStudents(...students) {
-//     console.log(students)
-// }
-
-// printStudents('Will', 'Mirsad', 'Muhsin', 'Trevor', 'Juan', 'Alice')
-
-// const fruits = ['orange', 'apple']
-
-// const copy = ['kiwi', ...fruits, 'banana']
-
-// const clone = {...data}
-// console.log(clone)
-
-// const data = {
-//     name: 'JD',
-//     age: 44
-// }
-
-// const { name, age, blah } = data
-
-// console.log(data)
-
-function someFunc({ age }, str, num) {
-    console.log(num)
-}
-
-someFunc({ name: 'Bob', age: 99 }, 'another', 15)
+  
 
 
 
