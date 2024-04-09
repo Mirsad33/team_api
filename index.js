64062ed1143c673ef422287e3d4b65fb59da7fb1
Mirsad33 <mabedinoski3387@gmail.com>
2024-04-09 10:13:20 -0400

    inquirer basic

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const { print, add } = require('./lib/studentTools.js')
-const dayjs = require('dayjs')
+const inquirer = require('inquirer')
 
-const studentName = process.argv[2]
-
-if (studentName === 'print') {
-  print()
-} else {
-  const date = dayjs().format('MM/DD/YYYY')
-
-  add(studentName + ' - ' + date)
-}
-
+inquirer.prompt([
+  {
+    name: 'age',
+    message: 'Please type your age'
+  },
+  {
+    name: 'full_name',
+    message: 'Please type your full name'
+  }
+]).then((answerObj) => {
+  console.log(answerObj)
+})
 
 
 
