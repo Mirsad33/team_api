3d337ba1cdb94258ebc6eb9d4e84a3498236f014
Mirsad33 <mabedinoski3387@gmail.com>
2024-04-12 11:43:14 -0400

    finished promises-detailed

--- index.js
+++ index.js
@@ -1,5 +1,61 @@
 
 
+const data = {
+    name: 'JD',
+    age: 44
+}
+
+function handlesSomeTask(isCool) {
+    return new Promise((resolve, reject) => {
+        if (isCool) {
+            setTimeout(() => {
+            resolve()
+            }, 5000);
+        } else {
+            reject()
+        }
+        
+     })
+    }
+
+handlesSomeTask(true)
+    .then(() => {
+        console.log('all good')
+    })
+    .then(() => {
+        console.log('two')
+    })
+    .then(() => {
+        console.log('three')
+    })
+    .catch(() => {
+        console.log('all bad')
+    })
+
+
+
+
+
+// class Prom {
+//     then(cb) {
+//     // Wait until some code runs and completes before calling the callback
+//         setTimeout(() => {
+//             cb()
+//         }, 3000)
+//     }
+
+//     catch(cb) {
+
+//     }
+// }
+
+// const prom = new Prom((resolve, reject) => {
+
+// })
+
+// prom.then(() => {
+//     console.log('callback called')
+// })
 
 
 
