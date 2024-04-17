4ba9ad84fe08c949bf1e8dacbda0aa02e093a7f2
Mirsad33 <mabedinoski3387@gmail.com>
2024-04-17 13:27:36 -0400

    fixed

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,9 +1,17 @@
 const router = require('express').Router()
 const { v4: generateID } = require('uuid')
-const data = require('../db/data')
+const fs = require('fs/promises')
+
+
+async function getData() {
+    const data = await fs.readFile('./db/users.json', 'utf8')
+
+    return JSON.parse(data)
+}
 
 // GET all users
-router.get('/users', (requestObj, resposnseObj) => {
+router.get('/users', async (requestObj, resposnseObj) => {
+    const data = await getData()
     const nameQuery = requestObj.query.name?.toLowerCase()
 
     if (nameQuery) {
@@ -27,17 +35,35 @@ router.get('/users/form', (requestObject, responseObject)=>{
     responseObj.json(user || {message: 'User not found by that ID'})
     })
     // POST  add a user
-router.post('/users', (requestObject, responseObject) => {
-    const id = generateID
+router.post('/users', async (requestObject, responseObject) => {
+    const id = generateID()
+    const data = await getData()
 
     data.push({
         ...requestObject.body,
         id: id
     })
 
+    await fs.writeFile('./db/users.json', JSON.stringify(data, null, 2))
+
     responseObject.json({
         message: 'User has been added'
     }) 
+})
+
+// DELETE a user
+
+router.delete('/users/:id', async (requestObj, responseObject) => {
+    const users = await getData()
+    const id = requestObj.params.id 
+
+    const filtered = users.filter(uObj => uObj.id !== id)
+
+    await fs.writeFile('./db/users.json', JSON.stringify(filtered, null, 2))
+
+    responseObject.json({
+        message: `User with ID of ${id} deleted successfully!`
+    })
 })
 
 module.exports = router
