11a3999f1eee45efdf418d29b4b3b31132e03ce2
Mirsad33 <mabedinoski3387@gmail.com>
2024-04-19 10:17:34 -0400

    finished postgres-basics

---
+++ query.sql
@@ -0,0 +1,40 @@
+
+
+SELECT
+    students.id AS student_id,
+    CONCAT(first_name, ' ', last_name) AS full_name,
+    course_id,
+    name,
+    type
+FROM students
+LEFT JOIN courses
+ON students.course_id = courses.id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- UPDATE users SET password = 'newpass' WHERE id = 2;
+
+
+-- SELECT * FROM users ORDER BY id ; 
